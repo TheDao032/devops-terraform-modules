fc8dfdcc00ca1a11382e8602e0be0bc5d35b5589
TheDao032 <nthedao2705@gmail.com>
2024-11-27 19:58:47 +0700

    build: update output for vpc module

--- aws/vpc/outputs.tf
+++ aws/vpc/outputs.tf
@@ -2,20 +2,36 @@ output "azs" {
   value = join(",", local.azs)
 }
 
-output "id" {
-  value = module.vpc.vpc_id
+# output "id" {
+#   value = module.vpc.vpc_id
+# }
+#
+# output "igw_id" {
+#   value = module.vpc.igw_id
+# }
+#
+# output "public_subnets" {
+#   # value = join(",", [for subnet in aws_subnet.public : subnet.id])
+#   value = module.vpc.private_subnets
+# }
+#
+# output "private_subnets" {
+#   # value = join(",", [for subnet in aws_subnet.private : subnet.id])
+#   value = module.vpc.public_subnets
+# }
+
+output "azs" {
+  value = join(",", local.azs)
 }
 
-output "igw_id" {
-  value = module.vpc.igw_id
+output "id" {
+  value = aws_vpc.main.id
 }
 
 output "public_subnets" {
-  # value = join(",", [for subnet in aws_subnet.public : subnet.id])
-  value = module.vpc.private_subnets
+  value = join(",", [for subnet in aws_subnet.public : subnet.id])
 }
 
 output "private_subnets" {
-  # value = join(",", [for subnet in aws_subnet.private : subnet.id])
-  value = module.vpc.public_subnets
+  value = join(",", [for subnet in aws_subnet.private : subnet.id])
 }
