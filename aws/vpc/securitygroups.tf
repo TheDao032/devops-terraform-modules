b1faba3e901f9761f1b444d711a6e7580d2ee92e
TheDao032 <nthedao2705@gmail.com>
2024-11-27 19:23:02 +0700

    build: test vpc module

---
+++ aws/vpc/securitygroups.tf
@@ -0,0 +1,44 @@
+locals {
+  service_ports = ["22", "80", "443"]
+}
+
+resource "aws_security_group" "ec2" {
+  name        = "${var.environment}-ec2-sg"
+  description = "EC2 Security Groups"
+  vpc_id      = module.vpc.vpc_id
+
+  tags = merge(
+    { Name = "${var.environment}-ec2-sg" },
+    var.tags
+  )
+}
+
+resource "aws_vpc_security_group_ingress_rule" "ec2_ingress" {
+  security_group_id = aws_security_group.ec2.id
+
+  cidr_ipv4   = module.vpc.public_subnets_cidr_blocks
+  cidr_ipv6   = module.vpc.public_subnets_ipv6_cidr_blocks
+  from_port   = 0
+  ip_protocol = "-1"
+  to_port     = 0
+
+  tags = merge(
+    { Name = "${var.environment}-ec2-sg-ingress-rule" },
+    var.tags
+  )
+}
+
+resource "aws_vpc_security_group_egress_rule" "ec2_egress" {
+  security_group_id = aws_security_group.ec2.id
+
+  cidr_ipv4   = "0.0.0.0/0"
+  cidr_ipv6   = "::/0"
+  from_port   = 0
+  ip_protocol = "-1"
+  to_port     = 0
+
+  tags = merge(
+    { Name = "${var.environment}-ec2-sg-egress-rule" },
+    var.tags
+  )
+}
--- aws/vpc/vpc.tf
+++ aws/vpc/vpc.tf
@@ -2,6 +2,125 @@ locals {
   azs = sort(var.azs)
 }
 
+# resource "aws_vpc" "main" {
+#   cidr_block           = var.vpc_cidr_block
+#   enable_dns_hostnames = true
+#   enable_dns_support   = true
+#
+#   tags = merge(
+#     { Name = var.environment },
+#     var.tags
+#   )
+# }
+#
+# resource "aws_internet_gateway" "main" {
+#   vpc_id = aws_vpc.main.id
+#
+#   tags = merge(
+#     { Name = var.environment },
+#     var.tags
+#   )
+# }
+#
+# resource "aws_subnet" "public" {
+#   count             = length(var.public_cidr)
+#   vpc_id            = aws_vpc.main.id
+#   cidr_block        = var.public_cidr[count.index]
+#   availability_zone = local.azs[count.index]
+#
+#   tags = merge(
+#     { Name = "${var.environment}-public-${count.index}" },
+#     { "kubernetes.io/cluster/${var.environment}" = "shared" },
+#     { "kubernetes.io/role/elb" = "1" },
+#     var.tags
+#   )
+# }
+#
+# resource "aws_route_table" "public" {
+#   count  = length(var.public_cidr)
+#   vpc_id = aws_vpc.main.id
+#
+#   route {
+#     cidr_block = "0.0.0.0/0"
+#     gateway_id = aws_internet_gateway.main.id
+#   }
+#
+#   tags = merge(
+#     { Name = "${var.environment}-public-${count.index}" },
+#     var.tags
+#   )
+# }
+#
+# resource "aws_route_table_association" "public" {
+#   count = length(var.public_cidr)
+#
+#   subnet_id      = aws_subnet.public[count.index].id
+#   route_table_id = aws_route_table.public[count.index].id
+# }
+#
+#
+# resource "aws_subnet" "private" {
+#   count             = length(var.private_cidr)
+#   vpc_id            = aws_vpc.main.id
+#   cidr_block        = var.private_cidr[count.index]
+#   availability_zone = local.azs[count.index]
+#
+#   tags = merge(
+#     { Name = "${var.environment}-private-${count.index}" },
+#     { "kubernetes.io/cluster/${var.environment}" = "shared" },
+#     { "kubernetes.io/role/internal-elb" = "1" },
+#     var.tags
+#   )
+# }
+#
+# resource "aws_eip" "nat" {
+#   count = length(var.public_cidr)
+#   vpc   = true
+#
+#   tags = merge(
+#     { Name = "${var.environment}-NAT-${count.index}" },
+#     var.tags
+#   )
+#
+#   depends_on = [aws_internet_gateway.main]
+# }
+#
+# resource "aws_nat_gateway" "main" {
+#   count         = length(var.public_cidr)
+#   allocation_id = aws_eip.nat[count.index].id
+#   subnet_id     = aws_subnet.public[count.index].id
+#
+#   tags = merge(
+#     { Name = "${var.environment}-${count.index}" },
+#     var.tags
+#   )
+#
+#   # To ensure proper ordering, it is recommended to add an explicit dependency
+#   # on the Internet Gateway for the VPC.
+#   depends_on = [aws_internet_gateway.main]
+# }
+#
+# resource "aws_route_table" "private" {
+#   count  = length(var.public_cidr)
+#   vpc_id = aws_vpc.main.id
+#
+#   route {
+#     cidr_block     = "0.0.0.0/0"
+#     nat_gateway_id = aws_nat_gateway.main[count.index].id
+#   }
+#
+#   tags = merge(
+#     { Name = "${var.environment}-private-${count.index}" },
+#     var.tags
+#   )
+# }
+#
+# resource "aws_route_table_association" "private" {
+#   count = length(var.public_cidr)
+#
+#   subnet_id      = aws_subnet.private[count.index].id
+#   route_table_id = aws_route_table.private[count.index].id
+# }
 
 module "vpc" {
   source = "terraform-aws-modules/vpc/aws"
