ed23c70db8435860620225ccfb391002d02a9cbd
TheDao032 <nthedao2705@gmail.com>
2024-12-01 21:42:13 +0700

    build: update ec2

--- aws/ec2/main.tf
+++ aws/ec2/main.tf
@@ -6,15 +6,20 @@ locals {
       core_count       = 1
       threads_per_core = 2
     },
-  ]
-
-  private_ec2_instances = [
     {
       instance_type    = "t3.medium"
       core_count       = 1
       threads_per_core = 2
     },
   ]
+
+  # private_ec2_instances = [
+  #   {
+  #     instance_type    = "t3.medium"
+  #     core_count       = 1
+  #     threads_per_core = 2
+  #   },
+  # ]
 }
 
 resource "aws_key_pair" "k3s_cluster" {
@@ -40,24 +45,23 @@ resource "aws_instance" "public_instances" {
     { Name = "public-ec2-${var.environment}-${count.index}" },
     var.tags
   )
-
 }
 
-resource "aws_instance" "private_instances" {
-  count                  = length(local.private_ec2_instances)
-  ami                    = data.aws_ami.amazon_linux_2023.id
-  instance_type          = local.private_ec2_instances[count.index].instance_type
-  subnet_id              = var.private_subnet_id
-  vpc_security_group_ids = [var.private_sg_id]
-  key_name               = var.key_pair
-
-  cpu_options {
-    core_count       = local.private_ec2_instances[count.index].core_count
-    threads_per_core = local.private_ec2_instances[count.index].threads_per_core
-  }
-
-  tags = merge(
-    { Name = "private-ec2-${var.environment}-${count.index}" },
-    var.tags
-  )
-}
+# resource "aws_instance" "private_instances" {
+#   count                  = length(local.private_ec2_instances)
+#   ami                    = data.aws_ami.amazon_linux_2023.id
+#   instance_type          = local.private_ec2_instances[count.index].instance_type
+#   subnet_id              = var.private_subnet_id
+#   vpc_security_group_ids = [var.private_sg_id]
+#   key_name               = var.key_pair
+#
+#   cpu_options {
+#     core_count       = local.private_ec2_instances[count.index].core_count
+#     threads_per_core = local.private_ec2_instances[count.index].threads_per_core
+#   }
+#
+#   tags = merge(
+#     { Name = "private-ec2-${var.environment}-${count.index}" },
+#     var.tags
+#   )
+# }
