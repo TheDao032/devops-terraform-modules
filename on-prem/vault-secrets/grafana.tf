855568fd842b0acb51aacfb45b5b4a99d7de9b36
TheDao032 <nthedao2705@gmail.com>
2024-12-05 17:06:21 +0700

    fix: update vault-secrets module

--- on-prem/vault-secrets/grafana.tf
+++ on-prem/vault-secrets/grafana.tf
@@ -6,6 +6,16 @@ locals {
     } if length({ for k, v in creds : k => v if contains(split(" ", v), "_RANDOM_") }) > 0
   }
 
+  flattened_grafana_secrets_parameters = flatten([
+    for path, creds in local.grafana_secrets_parameters : [
+      for key, value in creds : {
+        path  = path
+        key   = key
+        value = value
+      }
+    ]
+  ])
+
   # Generate resolved secrets from random_password
   grafana_secrets_resolve_parameters = {
     for path, creds in local.grafana_secrets_parameters : path => {
@@ -23,9 +33,23 @@ locals {
 
 }
 
+# resource "random_password" "grafana_secrets" {
+#   for_each         = { for path, creds in local.grafana_secrets_parameters : "${path}_${keys(creds)[0]}" => creds }
+#   length           = regex("[0-9]+", each.value[keys(each.value)[0]])
+#   override_special = "!()-_=+"
+#
+#   lifecycle {
+#     ignore_changes = [override_special]
+#   }
+# }
+
 resource "random_password" "grafana_secrets" {
-  for_each         = { for path, creds in local.grafana_secrets_parameters : "${path}_${keys(creds)[0]}" => creds }
-  length           = regex("[0-9]+", each.value[keys(each.value)[0]])
+  for_each = {
+    for secret in local.flattened_grafana_secrets_parameters :
+    "${secret.path}_${secret.key}" => secret
+  }
+
+  length           = regex("[0-9]+", each.value.value)
   override_special = "!()-_=+"
 
   lifecycle {
