b0e067c769709006e61cfe1f9d2a0cdb2ff7346f
TheDao032 <nthedao2705@gmail.com>
2025-02-28 13:58:13 +0700

    build: update loki modules

--- on-prem/loki/outputs.tf
+++ on-prem/loki/outputs.tf
@@ -1,3 +1,3 @@
 output "internal_loki_server" {
-  value = "${var.loki_helm_release_name}.${var.namespace}.svc.cluster.local"
+  value = "${var.loki_helm_release_name}-query-frontend.${var.namespace}.svc.cluster.local"
 }
