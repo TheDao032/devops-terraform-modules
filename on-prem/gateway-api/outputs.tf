ee55151918c6c29f306f721179add7bcb055015b
TheDao032 <nthedao2705@gmail.com>
2025-03-15 13:23:17 +0700

    feature: add gateway api module

--- on-prem/gateway-api/outputs.tf
+++ on-prem/gateway-api/outputs.tf
@@ -1,6 +1,6 @@
-output "traefik_gateway_class_name" {
-  value = local.traefik_gateway_class_name
-}
+# output "traefik_gateway_class_name" {
+#   value = local.traefik_gateway_class_name
+# }
 
 output "gateway_api_crds_raw" {
   value = data.http.gateway_api_crds_raw
