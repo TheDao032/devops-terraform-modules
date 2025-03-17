1657321c070578d6cc669c043ac4a16d7fe0186c
TheDao032 <nthedao2705@gmail.com>
2025-03-17 22:38:56 +0700

    build: update common module

--- on-prem/common/traefik.tf
+++ on-prem/common/traefik.tf
@@ -1,7 +1,7 @@
 locals {
-  traefik_gc_file                       = "${path.module}/templates/traefik/gc/traefik_gc.yml.tftpl"
-  traefik_dashboard_ingroute_value_file = "${path.module}/templates/traefik/ings/dashboard_ingroute.yml.tftpl"
-  traefik_gc_name                       = "traefik"
+  traefik_gc_file                    = "${path.module}/templates/traefik/gc/traefik_gc.yml.tftpl"
+  traefik_dashboard_gateway_api_file = "${path.module}/templates/traefik/gateway-api/dashboard_gatewap_api.yml.tftpl"
+  traefik_gc_name                    = "traefik"
 
   # traefik_dashboard_svc_value_file      = ""
   # traefik_dashboard_ing_value_file      = ""
@@ -29,18 +29,30 @@ locals {
 #   )
 # }
 
-resource "kubectl_manifest" "traefik_dashboard_ingroute" {
-  yaml_body = templatefile(
-    local.traefik_dashboard_ingroute_value_file,
-    {
-      ingress_route_name = var.traefik_dashboard_ingroute_name
-      namespace          = var.traefik_conf.namespace
-    }
-  )
-}
+# resource "kubectl_manifest" "traefik_dashboard_ingroute" {
+#   yaml_body = templatefile(
+#     local.traefik_dashboard_ingroute_value_file,
+#     {
+#       ingress_route_name = var.traefik_dashboard_ingroute_name
+#       namespace          = var.traefik_conf.namespace
+#     }
+#   )
+# }
 
 resource "kubectl_manifest" "gateway_class" {
   yaml_body = templatefile(local.traefik_gc_file, {
     gateway_class_name = local.traefik_gc_name
   })
+}
+
+resource "kubectl_manifest" "traefik_dashboard_gateway_api" {
+  yaml_body = templatefile(
+    local.traefik_dashboard_ingroute_value_file,
+    {
+      gateway_class_name = local.traefik_gc_name
+      namespace          = var.traefik_conf.namespace
+    }
+  )
+
+  depends_on = [kubectl_manifest.gateway_class]
 }
