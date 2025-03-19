fe688ab0302e3e4a403fe29d5547f61c6ee3b4de
TheDao032 <nthedao2705@gmail.com>
2025-03-20 00:14:58 +0700

    build: update k3s, helm, router module

--- on-prem/helm/main.tf
+++ on-prem/helm/main.tf
@@ -2,8 +2,20 @@ locals {
   value_file = "${path.module}/charts/${var.name}/values.yml.tftpl"
   chart      = var.chart == null ? null : "${path.module}/charts/${var.name}"
 
+  sc_value_file = "${path.module}/charts/${var.name}/templates/sc.yml.tftpl"
+  sc_created    = fileexists(local.sc_value_file) ? var.enabled : 0
+
   # serviceaccount_file = "${path.module}/charts/${var.name}/serviceaccount.json"
   # iam_role_created    = fileexists(local.serviceaccount_file) ? var.enabled : 0
+}
+
+resource "kubectl_manifest" "sc" {
+  count = local.sc_created
+
+  yaml_body = templatefile(local.sc_value_file, {
+    sc_name   = var.parameters.values.sc_name
+    namespace = var.namespace
+  })
 }
 
 resource "helm_release" "main" {
